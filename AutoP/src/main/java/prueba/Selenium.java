d275a6438eb11573e6323d1fc055da52c1277d94
carlomtz89 <113971060+carlomtz89@users.noreply.github.com>
2024-10-03 01:08:24 -0600

    Update Selenium.java
    COMMIT DESDE GHUB

--- AutoP/src/main/java/prueba/Selenium.java
+++ AutoP/src/main/java/prueba/Selenium.java
@@ -7,7 +7,7 @@ public class Selenium {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
+		driver.get("https://www.facebook.com");
 
 	}
 
